73d4622289afce984784bb0a61e75307fcf0df1f
Mikaye1 <mikayel.abrahamyan@gmail.com>
2021-08-15 17:40:37 -0700

    Bank Account

--- 02-java-oop/07-bank-account/BankAccountTester.java
+++ 02-java-oop/07-bank-account/BankAccountTester.java
@@ -1,10 +1,24 @@
 public class BankAccountTester {
     public static void main(String[] args) {
-        BankAccount b = new BankAccount();
+        System.out.println(BankAccount.numberOfAccounts);
+        System.out.println(BankAccount.totalAmount);
+        BankAccount b= new BankAccount();
 
-        b.setChecking(50000);
-        double check = b.getChecking();
-        System.out.println(check);
+        b.checkingDeposit(5000);
+        b.savingDeposit(5000);
+
+        System.out.println(b.getChecking());
+        b.savingDeposit(5000);
+
+        System.out.println(b.getSaving());
+        b.checkingWithdrawal(4000);
+
+        System.out.println(b.getChecking());
+        b.checkingWithdrawal(1000);
+
+        System.out.println(b.getChecking());
+        b.savingWithdrawal(100);
+
+        b.displayBalances();
     }
 }
-    
